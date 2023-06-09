766549f461aa0c18a4d6768550a5c09190df5c71
akshaydeshmukh2455 <akshdeshmukh123@gmail.com>
2023-06-09 11:32:42 +0530

    first commit

---
+++ src/app/module/module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComComponent } from './com/com.component';
+
+
+
+@NgModule({
+  declarations: [
+    ComComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModuleModule { }
