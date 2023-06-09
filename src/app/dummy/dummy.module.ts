150eb40172f8733866786e983892efdeed91e298
akshaydeshmukh2455 <akshdeshmukh123@gmail.com>
2023-06-09 12:20:10 +0530

    "student list added"

--- src/app/dummy/dummy.module.ts
+++ src/app/dummy/dummy.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DummycomponentComponent } from './dummycomponent/dummycomponent.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 
 
 @NgModule({
   declarations: [
-    DummycomponentComponent
+    DummycomponentComponent,
+    StudentListComponent
   ],
   imports: [
     CommonModule
